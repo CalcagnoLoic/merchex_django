72c155a0f65cbf3b109cc087fa1157c3372631bf
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-06-17 14:03:13 +0200

    backend part

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band
+from listings.models import Title
+
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name_title', 'type', 'sold', 'band')
+
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Title, TitleAdmin)
