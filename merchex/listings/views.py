bc488110747920144be903701a25bb96e824c462
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-06-18 12:48:08 +0200

    add form

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+
 from listings.models import Band
 from listings.models import Title
+from listings.forms import ContactUsForm
 
 
 def band_list(request):
@@ -34,5 +36,25 @@ def listings_detail(request, id):
 
 
 def contact(request):
-    return render(request, 'listings/contact.html')
+    #print('La méthode de requête est : ', request.method)
+    #print('Les données POST sont : ', request.POST)
 
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                subject=f"Message from {form.cleaned_data['name'] or 'anonyme'} via MerchEx Contact Us form",
+                message=form.cleaned_data["message"],
+                from_email=form.cleaned_data['email'],
+                recipient_list=['admin@merchex.xyz']
+            )
+        return redirect('email_sent.html')
+    else:
+        form = ContactUsForm()
+
+    return render(request, 'listings/contact.html',
+                  {'form': form})
+
+
+def email_sent(request):
+    return render(request, 'listings/email_sent.html')
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.band_list, name="band-list"),
     path('bands/<int:id>/', views.band_detail, name="band-detail"),
-    path('about-us/', views.about),
-    path('contact-us', views.contact),
+    path('about-us/', views.about, name="about"),
+    path('contact-us', views.contact, name="contact"),
     path('annonces', views.listings_list, name='listings-list'),
-    path('title/<int:id>/', views.listings_detail, name="listings-detail")
+    path('title/<int:id>/', views.listings_detail, name="listings-detail"),
+    path('email_sent.html', views.email_sent)
 
 ]
